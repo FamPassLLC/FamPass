8b0106908bba1ae63b3135d1229b588ec50e3f00
Derek Lam <derekquoc@gmail.com>
2020-12-19 17:47:27 -0800

    Set up css/login and family page

--- client/App.jsx
+++ client/App.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import { Switch, Route, Link } from 'react-router-dom';
 import { render } from "react-dom";
-import Family from './components/Family';
+import FamilyPage from './components/FamilyPage';
 
 class App extends Component {
     constructor() {
@@ -15,10 +15,27 @@ class App extends Component {
     
     render() {
         return (
+        <Switch> 
+          <Route path='/'>
           <div> Julie is the best interior designer!
               <div> Derek Lam models in wedding dresses!</div>
           </div>
+          </Route>
+
+          <Route path='/'>
+          <div>
+            <LoginPage />
+          </div>  
+          </Route>
+
+          <Route path='/'>
+          <div>
+            <FamilyPage />
+          </div>  
+          </Route>
           
+
+        </Switch>  
         );
     };
 
---
+++ client/components/FamilyPage.jsx
@@ -0,0 +1,8 @@
+import React, { Component } from "react";
+import { Switch, Route, Link } from 'react-router-dom';
+import { render } from "react-dom";
+
+const FamilyPage = [];
+
+
+export default FamilyPage;
