0ea9de2bfb0e222f5875ff8c1aceac7269b6bab9
Linh Tran <linhtanl51@gmail.com>
2020-12-22 16:16:18 -0600

    finish FamilyPage UI

--- client/index.js
+++ client/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { render } from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App.jsx';
+import styles from './componentStyles/style.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 render(
   <BrowserRouter>
