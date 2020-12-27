38c99676a6ace5e686969a1a04f5e4c7149abd0b
Alex Wolinsky <alexwolinsky1@gmail.com>
2020-12-27 10:13:01 -0800

    working on adding family passwords

--- server/controllers/familiesController.js
+++ server/controllers/familiesController.js
@@ -1,4 +1,7 @@
+const bcrypt = require('bcrypt');
 const db = require('../models/dbModels');
+
+const saltRounds = 13;
 
 const familiesController = {};
 
@@ -26,8 +29,8 @@ familiesController.getFamilies = (req, res, next) => {
 
 // post request to create a family
 familiesController.addFamily = (req, res, next) => {
-  // request body should include family name
-  const { family_name } = req.body;
+  // request body should include family name and family password
+  const { family_name, family_password } = req.body;
   // first, check whether a family with name already exists
   db.query('SELECT * FROM families WHERE (family_name = $1)', [family_name])
     .then((data) => {
@@ -35,14 +38,19 @@ familiesController.addFamily = (req, res, next) => {
         res.locals.status = 'family already exists';
         return next();
       } else {
-        const queryString = `INSERT INTO families (family_name) VALUES ($1)`;
-        const values = [family_name];
+        // encrypt password using bcrypt
+        bcrypt.hash(family_password, saltRounds, (err, hash) => {
+          const hashPassword = hash;
+          const values = [family_name, hashPassword];
+        const queryString = `INSERT INTO families (family_name, family_password) VALUES ($1, $2)`;
         // query
-        db.query(queryString, values).then((data) => {
-          res.locals.status = 'family created';
-          return next();
+        db.query(queryString, values)
+          .then((data) => {
+            res.locals.status = 'family created';
+            return next();
+          });
         });
-      }
+      };
     })
     .catch((err) => next({ err }));
 };
@@ -90,15 +98,20 @@ familiesController.deleteFamily = (req, res, next) => {
 
 // adds a user to a family - pass local_user and family_name
 familiesController.addMember = (req, res, next) => {
-  // get data from body and put into variables - body will include family name and local username of user to add
-  const { family_name, local_user } = req.body;
+  // get data from body and put into variables - body will include family name, family password, and local username of user to add
+  const { family_name, family_password, local_user } = req.body;
   // first queries - get family_id and user_id corresponding to input data
   // get _id from families table
-  const firstQuery1 = `SELECT _id FROM families WHERE (family_name = $1)`;
+  const firstQuery1 = `SELECT _id, family_password FROM families WHERE (family_name = $1)`;
   const firstQuery1Value = [family_name];
   db.query(firstQuery1, firstQuery1Value)
     .then((data) => {
       const family_id = data.rows[0]._id;
+      // verify family password
+      bcrypt.compare(family_password, data.rows[0].family_password, (err, result) => {
+        if (result === true) res.locals.
+      })
+
       // second portion of first query: get _id from local_users table
       const firstQuery2 = `SELECT _id FROM local_users WHERE (username = $1)`;
       const firstQuery2Value = [local_user];
